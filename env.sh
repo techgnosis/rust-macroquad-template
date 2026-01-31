34225708badc9ace7ebf8e3a963613e00df7979a
James Musselwhite <james.musselwhite@gmail.com>
2026-01-31 12:33:25 -0800

    ssh in the container

--- env.sh
+++ env.sh
@@ -8,6 +8,7 @@ podman run \
 -v ~/.claude.json:/home/james/.claude.json:Z \
 -v ~/.claude:/home/james/.claude:Z \
 -v ~/.gitconfig:/home/james/.gitconfig:Z \
+-v ~/.ssh:/home/james/.ssh:ro,Z \
 -v .:/workspace:Z \
 --userns=keep-id \
 rust-claude-beads:1 bash
